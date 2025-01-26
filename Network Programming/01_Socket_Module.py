7fb38e3423378b761634814f949113feec19683f
Santiago Cruz <161742373+santiago-cruzlopez@users.noreply.github.com>
2025-01-25 19:00:46 -0500

    Update 01_Socket_Module.py

--- Network Programming/01_Socket_Module.py
+++ Network Programming/01_Socket_Module.py
@@ -1,22 +1,30 @@
 import socket
 import ipaddress  
 from bs4 import BeautifulSoup
+
 # Create a socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 # Connect to a remote host
 s.connect(('www.example.com', 80))
+
 # Send data
 s.send(b'GET / HTTP/1.1\r\nHost: www.example.com\r\n\r\n')
+
 # Receive data
 data1 = s.recv(1024)
 html_content = data1.decode()
+
 # Parse the HTML
 soup = BeautifulSoup(html_content, 'html.parser')
+
 # Extract the HTML code
 html_code = soup.prettify()
 print(html_code)
+
 # Close the socket
 s.close()
+
 #Client-Server Model
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('localhost', 12345))
