e403949d63b35cc85d23f0c0889cee8fff905391
Santiago Cruz <byte.alpha.tech@gmail.com>
2025-06-24 10:36:04 -0400

    OpenCV+VLC

--- Broadcasting_Media/2.SDI_OpenCV.py
+++ Broadcasting_Media/2.SDI_OpenCV.py
@@ -1,82 +1,76 @@
-// https://stackoverflow.com/questions/29638812/capture-frame-with-opencv-using-blackmagic-card-intensity-shuttle
-
-import vlc,cv2
+import vlc
+import cv2
 import ctypes
 import time
 import sys
 import numpy as np
-
 from PIL import Image
 
-
-pl = vlc.MediaPlayer('dshow://')
-
+# VLC video resolution
 VIDEOWIDTH = 1280
 VIDEOHEIGHT = 720
+BYTES_PER_PIXEL = 4  # for RV32 (RGBA)
 
-# size in bytes when RV32
-size = VIDEOWIDTH * VIDEOHEIGHT * 4
-# allocate buffer
-buf1 = (ctypes.c_ubyte * size)()
-buf2 = (ctypes.c_ubyte * size)()
-# get pointer to buffer
+# Allocate two frame buffers
+frame_size = VIDEOWIDTH * VIDEOHEIGHT * BYTES_PER_PIXEL
+buf1 = (ctypes.c_ubyte * frame_size)()
+buf2 = (ctypes.c_ubyte * frame_size)()
+
+# Global buffer pointer
 buf_p = ctypes.cast(buf1, ctypes.c_void_p)
 
-# global frame (or actually displayed frame) counter
-framenr = 0
-test=[]
-# vlc.CallbackDecorators.VideoLockCb is incorrect
+# Initialize global variables
+frame_number = 0
+last_frame = np.zeros((VIDEOHEIGHT, VIDEOWIDTH, 4), dtype=np.uint8)
+
+# Lock callback function
 CorrectVideoLockCb = ctypes.CFUNCTYPE(ctypes.c_void_p, ctypes.c_void_p, ctypes.POINTER(ctypes.c_void_p))
 
 @CorrectVideoLockCb
 def _lockcb(opaque, planes):
-    print "lock"
+    global buf_p, buf1, buf2, frame_size
+    # Copy buf1 to buf2 to allow reading outside of lock
+    ctypes.memmove(buf2, buf1, frame_size)
     planes[0] = buf_p
-    ctypes.memmove(buf2, buf1, size)
+    return None
 
-    #time.sleep(1)
-
+# Display callback function
 @vlc.CallbackDecorators.VideoDisplayCb
-def _display(opaque, picture):
+def _displaycb(opaque, picture):
+    global frame_number, last_frame
 
-    global framenr,test
-
+    # Convert buffer to PIL Image
     img = Image.frombuffer("RGBA", (VIDEOWIDTH, VIDEOHEIGHT), buf2, "raw", "BGRA", 0, 1)
-    #img.save('img{}.png'.format(framenr))
-    #print "display {}".format(framenr)
-    # shouldn't do this here! copy buffer fast and process in our own thread, or maybe cycle
-    # through a couple of buffers, passing one of them in _lockcb while we read from the other(s).
-        #img.save('img{}.png'.format(framenr))
-    #img=cv2.cvtColor(np.array(img), cv2.COLOR_BGRA2RGB)
-    frame_CV=np.array(img)
-    framenr+=1    
-    print "frame",framenr,frame_CV.shape        
-    test=frame_CV
-    font = cv2.FONT_HERSHEY_SIMPLEX    
-    cv2.putText(test,"Test",(50,50), font, 0.7,(250,20,30),1,cv2.LINE_AA)
-    #cv2.imshow("Test",test)
-    #cv2.waitKey(1)
-    #if framenr==10:      
-        #cv2.imshow("Test",test)        
-    #print "OpenCV"
+    frame_np = np.array(img)
 
-#------------------------------------------------------------------------------    
-# MAIN CYCLE
-#------------------------------------------------------------------------------
-vlc.libvlc_video_set_callbacks(pl, _lockcb, None, _display, None)
-pl.video_set_format("RV32", VIDEOWIDTH, VIDEOHEIGHT, VIDEOWIDTH * 4)
-pl.play()
-#10 seconds are necessary to consolidate memory buffer exchange and library 
-#syncronization
+    # Draw something on the frame
+    cv2.putText(frame_np, f"Frame {frame_number}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                0.7, (250, 20, 30), 2, cv2.LINE_AA)
 
-time.sleep(10)
-while True:
+    # Save frame for display
+    last_frame = frame_np
+    frame_number += 1
 
+# Create VLC player and set media
+player = vlc.MediaPlayer("dshow://")  # Use 'v4l2://' on Linux
 
-    cv2.imshow("Test",test)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-pl.stop()
-cv2.destroyAllWindows()
-#time.sleep(10)
-#------------------------------------------------------------------------------
+# Set VLC callbacks and video format
+vlc.libvlc_video_set_callbacks(player, _lockcb, None, _displaycb, None)
+player.video_set_format("RV32", VIDEOWIDTH, VIDEOHEIGHT, VIDEOWIDTH * BYTES_PER_PIXEL)
+player.play()
+
+# Give the VLC player time to initialize
+time.sleep(2)
+
+# Main loop to display frames
+try:
+    while True:
+        if last_frame is not None:
+            cv2.imshow("VLC Frame", last_frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+finally:
+    player.stop()
+    cv2.destroyAllWindows()
+
+# https://stackoverflow.com/questions/29638812/capture-frame-with-opencv-using-blackmagic-card-intensity-shuttle
